733750606df2fab2b0fb9bdb582c99da3c8db4ba
Hady Baldé <hadybalde05@gmail.com>
2024-12-03 14:48:57 +0100

    public

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
-import { assets } from "../../assets";
+import { assets } from "../assets";
 
 const Product = () => {
   const { productId } = useParams();
