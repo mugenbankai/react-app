6c864b64c97d5040a71dd88b2354148183b8336b
Hady Baldé <hadybalde05@gmail.com>
2024-12-03 14:20:28 +0100

    public

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
-})
+  server: { port: 5173 }, // Port pour le développement
+  base: "/react-app/", // Remplace <nom-du-repo> par le nom de ton dépôt GitHub
+});
